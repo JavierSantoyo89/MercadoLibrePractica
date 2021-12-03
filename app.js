c8d598cd08e78278af62fe884c2f68d809424ea4
Javier Santoyo <javier.santoyo@yahoo.com>
2021-12-03 03:38:16 -0600

    Actualizacion global HTML y CSS

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const port = 1689;
 
 app.use(express.static('public'));
 
+
+/*! ------------------------ Seccion de rutas web dentro del dominio -------------------- */ 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+/* -------------------------------- Seccion de levantado del servidor -------------------- */
 app.listen(port,()=>{
     console.log('Servidor activado en el puerto: http://localhost:'+ port);
 });
+
